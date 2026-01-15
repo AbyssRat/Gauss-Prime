f52d6c9c396fa641e4ec2f115e55dade37d320d6
AbyssRat <yunothefurry@gmail.com>
2026-01-15 10:30:55 +0100

    7

---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <main>
+        <h1>Welcome to the Main Layout</h1>
+        {/* Additional content can go here */}
+        </main>
+    );
+    }
+export default Main;
